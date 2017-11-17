7da851d3066f4de62018ec18b06c8198fad57c4b
Becca <becca@lthorup.com>
2017-11-17 14:41:38 -0700

    Created new AdventureTest that works with Junit 5

--- src/AdventureTest.java
+++ src/AdventureTest.java
@@ -1,23 +1,16 @@
-import static org.junit.jupiter.api.Assertions
-;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-import junit.framework.Assert;
-
-public class AdventureTest {
+class AdventureTest {
 
 	@Test
-	public void test() {
-		
+	void test() {
 		Adventure adventure = new Adventure("test.txt");
 		
 		String cmd = "go south";
 		String expected = "Moving south...\r\n" + "You are at location 1,0";
 		String actual = adventure.executeCommand(cmd);
 		Assertions.assertEquals(expected, actual);
-		
-		
 	}
 
 }
